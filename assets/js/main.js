5deea2fc9a0d1475e4666b75d5d3e46001922124
Xiaoying Riley <xiaoying@3rdwavemedia.com>
2019-10-16 14:29:28 +0100

    closes: #7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+$(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+    
+        var itemWidth = $(this).data('level');
+        
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+        
+    });
+
+});
+
+
 jQuery(document).ready(function($) {
 
 
@@ -5,19 +20,7 @@ jQuery(document).ready(function($) {
     
     $('.level-bar-inner').css('width', '0');
     
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
-    });
+    
     
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
