6614f1b7b6711811a1c266d5fd201b48b574691a
Xiaoying Riley <xiaoying@3rdwavemedia.com>
2023-06-26 10:52:11 +0100

    updated tooltip js and HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
 "use strict";
 
-//Enable tooltips everywhere
-//var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-//var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//  return new bootstrap.Tooltip(tooltipTriggerEl)
-//})
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
